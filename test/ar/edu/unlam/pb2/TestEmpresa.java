14c76f3c2fa485894d432e72456c3e3968b0355b
Pablo2810 <pablorocha.2810@gmail.com>
2023-05-10 10:02:55 -0300

    Test

---
+++ test/ar/edu/unlam/pb2/TestEmpresa.java
@@ -0,0 +1,18 @@
+package ar.edu.unlam.pb2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestEmpresa {
+
+	@Test
+	public void queSePuedaCrearUnaEmpresa() {
+		
+	}
+	
+	@Test
+	public void queSePuedaCrearUnaCredencial() {
+		
+	}
+}
